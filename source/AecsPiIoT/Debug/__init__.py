58144ea757db23980f4e08d233228a0280608bc9
Lennie Wennerlund <lennie@lempa.se>
2016-10-16 01:37:26 +0200

    file fix

---
+++ source/AecsPiIoT/Debug/__init__.py
@@ -0,0 +1,27 @@
+
+import io
+import os
+import time
+import datetime
+from Settings import Settings
+
+def SendToLog(Text):
+    if (Settings.debugEnable):
+        if (Settings.debugToConsole):
+            print(Text)
+        if (Settings.debugToFile):
+            with open(Settings.debugfile, 'a') as file_:
+                file_.write(time.strftime("%H:%M:%S") + " | " + str(Text) + "\n")
+
+
+class Debug(object):
+    def Info(Text):
+        SendToLog("Info  | " + Text)
+    def Warning(Text):
+        SendToLog("Warn  | " + Text)
+    def Error(Text):
+        SendToLog("Error | " + Text)
+
+
+
+        
